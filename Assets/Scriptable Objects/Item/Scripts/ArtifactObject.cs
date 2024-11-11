5987f4f2afc8bc68c8a38a540f1644f2e68e7b05
Scott Do <dobao98123@gmail.com>
2024-11-11 11:17:40 -0500

    redo of everything items and inventory

---
+++ Assets/Scriptable Objects/Item/Scripts/ArtifactObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Artifact Object", menuName = "Inventory System/Items/Artifact")]
+public class ArtifactObject : ItemData
+{
+    public int value; /// Value of artifact
+
+    public void Awake()
+    {
+        type = ItemType.Artifact;
+    }
+}

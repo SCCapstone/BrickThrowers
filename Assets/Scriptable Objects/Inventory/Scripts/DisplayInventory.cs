b7ec1ebe7cbf59f5176046b7436a700f8c367ff4
Scott Do <dobao98123@gmail.com>
2024-11-17 20:05:45 -0500

    redo of inventory system foundation

--- Assets/Scriptable Objects/Inventory/Scripts/DisplayInventory.cs
+++ Assets/Scriptable Objects/Inventory/Scripts/DisplayInventory.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DisplayInventory : MonoBehaviour
 
 {
-    public InventoryObject inventory;
+    public InventoryObject inventory; // Hold inventory object to display
     public int X_SPACE_BETWEEN_ITEM;
+    public int NUMBER_OF_COLUMNS;
+    public int Y_SPACE_BETWEEN_ITEM;
+    public int X_START;
+    public int Y_START;
     Dictionary<InventorySlot, GameObject> itemDisplayed = new Dictionary<InventorySlot, GameObject>();
     // Start is called before the first frame update
     void Start()
@@ -20,14 +25,45 @@ public class DisplayInventory : MonoBehaviour
         UpdateDisplay();
     }
 
+    /// <summary>
+    /// Creates the display for the inventory slots of the User Interface and adds it into the dictionary
+    /// </summary>
     public void CreateDisplay()
     {
         for (int i = 0; i < inventory.Container.Count; i++)
         {
-            var obj = inventory.Container[i].item.prefab;
-            var go = Instantiate(obj, Vector3.zero, Quaternion.identity, transform);
-            go.GetComponent<RectTransform>().localPosition = GetPosition(i);
-            go.GetComponentInChildren<UnityEngine.UI.Text>().text = inventory.Container[i].amount.ToString("n0");
-            itemDisplayed.Add(inventory.Container[i], go);
+            var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+            obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+            obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            itemDisplayed.Add(inventory.Container[i], obj);
         }
     }
+    /// <summary>
+    /// Takes in an integer and returns the position of the item in the inventory
+    /// </summary>
+    /// <param name="i">The index of the item to create a slot for.</param>
+    /// <returns>The Vector3 position of the slot.</returns>
+    public Vector3 GetPosition(int i)
+    {
+        return new Vector3(X_START + (X_SPACE_BETWEEN_ITEM * (i % NUMBER_OF_COLUMNS)), Y_START +  (-Y_SPACE_BETWEEN_ITEM * (i / NUMBER_OF_COLUMNS)), 0f);
+    }
+    /// <summary>
+    /// Updates display of inventory during an update.
+    /// </summary>
+    public void UpdateDisplay()
+    {
+        for (int i = 0;i < inventory.Container.Count; i++)
+        {
+            if (itemDisplayed.ContainsKey(inventory.Container[i]))
+            {
+                itemDisplayed[inventory.Container[i]].GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+            } else
+            {
+                var obj = Instantiate(inventory.Container[i].item.prefab, Vector3.zero, Quaternion.identity, transform);
+                obj.GetComponent<RectTransform>().localPosition = GetPosition(i);
+                obj.GetComponentInChildren<TextMeshProUGUI>().text = inventory.Container[i].amount.ToString("n0");
+                itemDisplayed.Add(inventory.Container[i], obj);
+            }
+        }
+    }
+}

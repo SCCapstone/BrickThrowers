9b307ee9351f4526a07b0dd41787eaafc607e4be
Scott Do <dobao98123@gmail.com>
2025-02-01 14:13:43 -0500

    Rearrange files and tests for #118
    All script files have been moved to a folder name "Scripts" so that making an assembly definition can be easier without cyclic references. The behavior test has also been made in the folder "PlayTests".

---
+++ Assets/PlayTests/PlayerTests.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerTests
+{
+    private GameObject antidoteItem = Resources.Load<GameObject>("antidoteTest");
+    private GameObject playerPrefab = Resources.Load<GameObject>("TempPlayerTest");
+    private ItemDatabaseObject itemDatabase = Resources.Load<ItemDatabaseObject>("Item Database");
+
+    private GameObject playerObject;
+    private Player player;
+
+    private GameObject itemObject;
+    private GroundItem item;
+
+    [SetUp]
+    public void Setup()
+    {
+        CreatePlayer();
+        CreateItem();
+    }
+
+    private void CreatePlayer()
+    {
+        playerObject = GameObject.Instantiate(playerPrefab);
+        player = playerObject.GetComponent<Player>();
+
+    }
+
+    private void CreateItem()
+    {
+        itemObject = GameObject.Instantiate(antidoteItem);
+        item = itemObject.GetComponent<GroundItem>();
+
+    }
+
+    /*
+     * Test to determine if a player can pick up an item.
+     */
+    [UnityTest]
+    public IEnumerator PlayerCollectsEquipmentItem()
+    {
+        // Wait for user input to click key E.
+        yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E));
+
+        // Determine if the item is added into the inventory.
+        Assert.IsFalse(itemObject.activeSelf);
+        yield return null;
+        Assert.IsTrue(player.inventory.Container.Items.Count > 0);
+        Assert.AreEqual(itemDatabase.items[0].Id, player.inventory.Container.Items[0].item.Id);
+        Assert.AreEqual(itemDatabase.items[0].itemName, player.inventory.Container.Items[0].item.Name);
+
+
+
+
+    }
+}

113e120fc9d4d84808ebe5ced9a3d24a61757819
Scott Do <dobao98123@gmail.com>
2025-03-02 20:40:14 -0500

    fix all linking errors present

--- Assets/Scripts/VolumeSettings.cs
+++ Assets/Scripts/VolumeSettings.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
     [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider masterVolume;
     [SerializeField] private Slider musicSlider;
+
+    public void SetMasterVolume()
+    {
+        float volume = masterVolume.value;
+        myMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+    }
 
     public void SetMusicVolume()
     {
         float volume = musicSlider.value;
-        myMixer.SetFloat("MasterVolume", Mathf.Log10(volume) * 20);
+        myMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
 }

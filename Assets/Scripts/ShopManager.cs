95f5f6d1958e91332f654461f06fcc9d853cb654
Scott Do <dobao98123@gmail.com>
2025-04-05 17:25:24 -0400

    shop and item purchase script setup

--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
+    [SerializeField] private GameObject shopOverlay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        shopOverlay.SetActive(false);
     }
 
     // Update is called once per frame

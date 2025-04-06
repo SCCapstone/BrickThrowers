3545adc257a7cfe7b78f0d0b93a6bc919e92b465
Scott Do <dobao98123@gmail.com>
2025-04-06 12:34:09 -0400

    Shop item prefabs show items and pricing

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -1,18 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    /*
+     * A shop item shows the item picture, and the pricing.
+     * There is a button where the pricing is to purchase the item.
+     * When the button is clicked, the item is purchased.
+     * Send a signal to the shop manager to update the currency.
+     * So, create an action to signal the shop manager that the purchase was successful.
+     * Ensure that the item purchased appears in the player inventory in game scenes.
+     * Update the currency in the shop manager.
+     */
+
+    // Item related variables
+    private ItemClass item;
+
+    // Game objects
+    [SerializeField] private TextMeshProUGUI itemPrice;
+    [SerializeField] private Image image;
+
+    // Actions
+    public static event Action<int> onItemPurchased;
+
+    // The item that is being purchased.
+    private void ItemPurchased()
     {
-        
+        onItemPurchased?.Invoke(item.ReturnCost());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetItem(ItemClass item)
     {
-        
+        this.item = item;
+    }
+
+    // Change the text to match item cost.
+    public void SetItemCost()
+    {
+        itemPrice.text = item.ReturnCost().ToString();
+    }
+
+    public void SetSprite()
+    {
+        this.image.sprite = item.itemIcon;
     }
 }
--- Assets/Scripts/ShopManager.cs
+++ Assets/Scripts/ShopManager.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class ShopManager : MonoBehaviour
 {
+
+    // Game objects
     [SerializeField] private GameObject shopOverlay;
+    [SerializeField] private GameObject buyOptionPrefab;
+    [SerializeField] private GameObject contentDisplay;
+
+    // Database
+    [SerializeField] private ItemDatabaseObject purchaseDatabase;
+
+    // Currency manager
+    private CurrencyManager currency;
 
     // Start is called before the first frame update
     void Start()
     {
         shopOverlay.SetActive(false);
+        for (int i = 0; i < purchaseDatabase.itemsDatabase.Length; i++)
+        {
+            PopulateStore(i);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    /*
+     * Before the game loads, populate the store with the available purchasable items.
+     * 1. Create a prefab for the item.
+     * 2. Set that buy option prefab's item class to the item class in the database.
+     * 3. Add the buy option prefab to the content display.
+     * 4. Continue doing the above until all items are added to the store. Effectively going through the database.
+     */
+
+    private void PopulateStore(int i)
     {
-        
+        // Instantiate the prefab.
+        GameObject buyOption = Instantiate(buyOptionPrefab, contentDisplay.transform);
+
+        // Set the item class of the buy option prefab.
+        buyOption.GetComponent<ShopItem>().SetItem(purchaseDatabase.itemsDatabase[i]);
+
+        // Set the item cost of the buy option prefab.
+        buyOption.GetComponent<ShopItem>().SetItemCost();
+
+        // Set sprite
+        buyOption.GetComponent<ShopItem>().SetSprite();
     }
+
+
 }

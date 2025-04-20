4f5227c351decde7b028d00706dfd525dc93ac32
Scott Do <dobao98123@gmail.com>
2025-04-20 12:09:13 -0400

    escape key functionality achieved #204

---
+++ Assets/Scripts/EscapeKeyCancel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class EscapeKeyCancel : MonoBehaviour
+{
+    private PlayerInputActions controls;
+    private InputAction escape;
+
+    #region Setup Functions
+    private void Awake()
+    {
+        controls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        escape = controls.UI.Cancel;
+        escape.Enable();
+        escape.performed += OnCancel;
+    }
+
+    private void OnDisable()
+    {
+        escape.Disable();
+        escape.performed -= OnCancel;
+    }
+
+    private void OnCancel(InputAction.CallbackContext contex)
+    {
+        this.gameObject.SetActive(false);
+    }
+    #endregion
+}

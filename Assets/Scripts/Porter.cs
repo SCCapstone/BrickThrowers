8ce3355ac845a897bf807eb68be59a4467363a01
Scott Do <dobao98123@gmail.com>
2025-04-25 12:43:07 -0400

    porter now has an extra slot #130

--- Assets/Scripts/Porter.cs
+++ Assets/Scripts/Porter.cs
@@ -4,50 +4,55 @@ using UnityEngine;
 
 public class Porter : MonoBehaviour
 {
-    private object extraSlotItem; // The extra slot for the Porter variant!
+    // Slots and slot manager
+    [SerializeField] private GameObject slot;
+    [SerializeField] private GameObject slotHolder;
+    private const int MAX_SLOTS = 4;
+    private const int DEFAULT_SLOTS = 3;
+    private const float EXTRA_SLOT_WIDTH = 95f;
+    // 95 more width to make the visual look good
 
-    public bool HasExtraSlotItem => extraSlotItem != null;
-
-    // This variable is here to allow the porter to be disabled or enabled.
+    // Script enabling and disabling
     private bool enableClass = false;
 
     private void OnEnable()
     {
         enableClass = true;
+        SetPorterInventory();
+
     }
     private void OnDisable()
     {
         enableClass = false;
+        ResetPorterInventory();
     }
 
-    public bool AddToExtraSlot(object item)
+    private void SetPorterInventory()
     {
-        if (extraSlotItem == null) 
+        // Set the contraint count of the slotHolder's grid layout group to the porter slots
+        slotHolder.GetComponent<UnityEngine.UI.GridLayoutGroup>().constraintCount = MAX_SLOTS;
+        slotHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(slotHolder.GetComponent<RectTransform>().sizeDelta.x + EXTRA_SLOT_WIDTH, slotHolder.GetComponent<RectTransform>().sizeDelta.y);
+        // Instantiate a slot and add it to the slotHolder as a child of the game object.
+        // it should be the last child of the slotHolder
+        GameObject newSlot = Instantiate(slot, slotHolder.transform);
+        newSlot.name = "Porter Slot";
+        newSlot.transform.localScale = Vector3.one;
+        newSlot.transform.localPosition = Vector3.zero;
+        newSlot.transform.localRotation = Quaternion.identity;
+        newSlot.transform.SetAsLastSibling();
+
+
+    }
+    private void ResetPorterInventory()
+    {
+        // Reset all the changes from the SetPorterInventory function
+        slotHolder.GetComponent<UnityEngine.UI.GridLayoutGroup>().constraintCount = DEFAULT_SLOTS;
+        slotHolder.GetComponent<RectTransform>().sizeDelta = new Vector2(slotHolder.GetComponent<RectTransform>().sizeDelta.x - EXTRA_SLOT_WIDTH, slotHolder.GetComponent<RectTransform>().sizeDelta.y);
+        // Remove the last slot of the slotHolder
+        if (slotHolder.transform.childCount > 0)
         {
-            extraSlotItem = item;
-            Debug.Log("Item added to Porter's extra slot.");
-            return true;
+            Destroy(slotHolder.transform.GetChild(slotHolder.transform.childCount - 1).gameObject);
         }
-        Debug.Log("Extra slot is already occupied.");
-        return false;
     }
 
-    public object RemoveFromExtraSlot()
-    {
-        if (extraSlotItem != null)
-        {
-            object itemToReturn = extraSlotItem;
-            extraSlotItem = null;
-            Debug.Log("Item removed from Porter's extra slot.");
-            return itemToReturn;
-        }
-        Debug.Log("Extra slot is empty.");
-        return null;
-    }
-
-    // ths gets the item in the extra slot
-    public object GetExtraSlotItem()
-    {
-        return extraSlotItem;
-    }
 }

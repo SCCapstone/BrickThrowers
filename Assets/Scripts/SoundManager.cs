a74ad74df73edef409ce754eaecfce3a4e658771
Scott Do <dobao98123@gmail.com>
2025-04-27 21:28:35 -0400

    music changes consistently between scenes

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -2,7 +2,10 @@
 // SoundManager.cs - Manages the sound effects and background music for the game.
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour {
@@ -12,11 +15,11 @@ public class SoundManager : MonoBehaviour {
   [SerializeField] private AudioSource musicSource;
   [SerializeField] private AudioClip buttonClickSFX;
   [SerializeField] private AudioClip bgMusic;
+  [SerializeField] private AudioClip gameMusic;
 
-  void Start() {
-
-  }
-
+  private const string MAIN_MENU = "Main Menu";
+  private const string LOBBY = "Lobby Scene";
+  #region Setup
   private void Awake() {
     if (Instance != null && Instance != this) {
       Destroy(gameObject);
@@ -26,6 +29,13 @@ public class SoundManager : MonoBehaviour {
     DontDestroyOnLoad(gameObject);
     PlayBackgroundMusic(bgMusic);
   }
+  private void OnEnable() {
+    SceneManager.sceneLoaded += OnSceneLoaded;
+  }
+  private void OnDisable() {
+    SceneManager.sceneLoaded -= OnSceneLoaded;
+  }
+  #endregion
 
   public void PlayButtonClickSound() {
     if (audioSource != null && buttonClickSFX != null) {
@@ -65,4 +75,28 @@ public class SoundManager : MonoBehaviour {
   public void AssignButtonClickSound(Button button) {
     button.onClick.AddListener(PlayButtonClickSound);
   }
+  #region Music Switching Logic
+  /// <summary>
+  /// Switch music based on the scene loaded.
+  /// If lobby and main menu, the load the bgMusic.
+  /// Otherwise, load the gameMusic.
+  /// </summary>
+  /// <param name="scene"></param>
+  /// <param name="mode"></param>
+  private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, LoadSceneMode mode) {
+
+    if (scene.name == MAIN_MENU || scene.name == LOBBY) {
+      SetMenuAndLobbyMusic();
+    } else {
+      SetGameMusic();
+    }
+  }
+
+  private void SetGameMusic() {
+    PlayBackgroundMusic(gameMusic);
+  }
+  private void SetMenuAndLobbyMusic() {
+    PlayBackgroundMusic(bgMusic);
+  }
+  #endregion
 }
